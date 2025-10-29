7ca41e20b608f8c4172ad46ffcdb2677950fcb00
Longping Fu <lpfu46@gatech.edu>
2025-10-29 19:39:36 -0400

    data analysis for preliminary data

--- data_analysis/OGlycoTM_HEK293T_Retreat/Preliminary_Results.R
+++ data_analysis/OGlycoTM_HEK293T_Retreat/Preliminary_Results.R
@@ -9,6 +9,10 @@ Preliminary_Results <- read_tsv(
 ) |> 
   filter(str_detect(Entry.Name, 'HUMAN')) |> 
   filter(!is.na(O.Pair.Score)) |> 
+  # mutate(
+  #   PPM = Delta.Mass/Observed.Mass
+  # ) |> 
+  filter(PPM < 10, PPM > -10) |> 
   select(
     Spectrum,
     Peptide, Peptide.Length, Charge, Observed.Mass, Observed.M.Z, Delta.Mass, Hyperscore, Nextscore,
@@ -120,5 +124,260 @@ Localized_Nonlocalized_Results_OGalNAc <- tibble(
   number = c(137, 413)
 )
 
+# GO Enrichment Analysis
+library(tidyverse)
+library(clusterProfiler)
+library(org.Hs.eg.db)
 
+Total_Glycoprotein <- Preliminary_Results_bonafide |> 
+  dplyr::select(Protein.ID) |> 
+  pull()
 
+OGlcNAc_GLycoprotein <- Preliminary_Results_bonafide |> 
+  filter(Total.Glycan.Composition %in% c('HexNAt(1)', 'HexNAt(1)TMT6plex(1)')) |> 
+  dplyr::select(Protein.ID) |> 
+  pull()
+
+OGalNAc_GLycoprotein <- Preliminary_Results_bonafide |> 
+  filter(Total.Glycan.Composition %in% c('HexNAt(1)GAO_Methoxylamine(1)', 'HexNAt(1)GAO_Methoxylamine(1)TMT6plex(1)')) |> 
+  dplyr::select(Protein.ID) |> 
+  pull()
+
+OGlcNAc_GO <- enrichGO(
+  gene = OGlcNAc_GLycoprotein,
+  OrgDb = org.Hs.eg.db,
+  universe = Total_Glycoprotein,
+  keyType = 'UNIPROT',
+  ont = 'ALL',
+  pvalueCutoff = 1,
+  qvalueCutoff = 1
+)
+
+write_csv(
+  OGlcNAc_GO@result, file = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/OGlcNAc_GO.csv'
+)
+
+OGalNAc_GO <- enrichGO(
+  gene = OGalNAc_GLycoprotein,
+  OrgDb = org.Hs.eg.db,
+  universe = Total_Glycoprotein,
+  keyType = 'UNIPROT',
+  ont = 'ALL',
+  pvalueCutoff = 1,
+  qvalueCutoff = 1
+)
+
+write_csv(
+  OGalNAc_GO@result, file = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/OGalNAc_GO.csv'
+)
+
+# barplot
+library(tidyverse)
+
+GlycoPSM_Glycoprotein_combined <- bind_rows(
+  GlycoPSM_Glycoprotein_Results_OGlcNAc %>% mutate(modification = "O-GlcNAc"),
+  GlycoPSM_Glycoprotein_Results_OGalNAc %>% mutate(modification = "O-GalNAc")
+)
+
+GlycoPSM_Evidence_combined <- bind_rows(
+  Localized_Nonlocalized_Results_OGlcNAc %>% mutate(modification = "O-GlcNAc"),
+  Localized_Nonlocalized_Results_OGalNAc %>% mutate(modification = "O-GalNAc")
+)
+
+# Number of total glycoPSM and unique glycoprotein
+GlycoPSM_Glycoprotein_barplot <- GlycoPSM_Glycoprotein_combined |> 
+  ggplot(aes(x = category, y = number, fill = factor(modification, c('O-GlcNAc', 'O-GalNAc')))) +
+  geom_col(position = "dodge", width = 0.7) +
+  scale_fill_manual(values = c("O-GlcNAc" = "#1f77b4", "O-GalNAc" = "#ff7f0e")) +
+  ylim(0, 6000) +
+  labs(
+    x = NULL,
+    y = "Count",
+    fill = ""
+  ) +
+  geom_text(
+    aes(label = number), color = 'black', size = 3, position = position_dodge(width = 0.7), vjust = -0.1
+  ) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(size = 8, color = 'black', angle = 30, hjust = 1),
+    axis.text.y = element_text(size = 8, color = 'black'),
+    axis.title.y = element_text(size = 8, color = 'black'),
+    legend.position = "none"
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/Retreat2025/Figures/GlycoPSM_Glycoprotein_barplot.tif',
+  plot = GlycoPSM_Glycoprotein_barplot,
+  height = 2, width = 2, units = 'in', dpi = 300
+)
+
+# Number of glycoPSM with different levels of evidence
+GlycoPSM_Evidence_barplot <- GlycoPSM_Evidence_combined |> 
+  ggplot(aes(x = category, y = number, fill = factor(modification, c('O-GlcNAc', 'O-GalNAc')))) +
+  geom_col(position = "dodge", width = 0.7) +
+  scale_fill_manual(values = c("O-GlcNAc" = "#1f77b4", "O-GalNAc" = "#ff7f0e")) +
+  ylim(0, 5500) +
+  labs(
+    x = NULL,
+    y = "# of glycoPSM",
+    fill = ""
+  ) +
+  geom_text(
+    aes(label = number), color = 'black', size = 3, position = position_dodge(width = 0.7), vjust = -0.1
+  ) +
+  theme_classic() +
+  theme(
+    axis.text.x = element_text(size = 8, color = 'black', angle = 30, hjust = 1),
+    axis.text.y = element_text(size = 8, color = 'black'),
+    axis.title.y = element_text(size = 8, color = 'black'),
+    legend.position = "none"
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/Retreat2025/Figures/GlycoPSM_Evidence_barplot.tif',
+  plot = GlycoPSM_Evidence_barplot,
+  height = 2, width = 2, units = 'in', dpi = 300
+)
+
+# GO enrichment analysis of O-GalNAc
+library(tidyverse)
+
+OGalNAc_GO <- read_csv(
+  '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/OGalNAc_GO.csv'
+)
+
+barplot_GO_OGalNAc <- OGalNAc_GO |> 
+  filter(Description %in% c(
+    'endoplasmic reticulum membrane',
+    'protein folding',
+    'protein maturation',
+    'lysosome',
+    'establishment of protein localization',
+    'extracellular organelle'
+  )) |> 
+  ggplot() +
+  geom_bar(
+    aes(
+      x = fct_reorder(Description, -log10(pvalue)), 
+      y = -log10(pvalue)
+    ),
+    fill = '#ff7f0e', color = 'transparent', stat = 'identity'
+  ) +
+  labs(x = '', y = '') +
+  coord_flip() +
+  theme_classic() +
+  theme(
+    axis.text = element_text(size = 8, color = 'black')
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/Retreat2025/Figures/barplot_GO_OGalNAc.tif',
+  plot = barplot_GO_OGalNAc,
+  height = 2, width =  3, units = 'in', dpi = 300
+)
+
+# GO enrichment analysis of O-GlcNAc
+library(tidyverse)
+
+OGlcNAc_GO <- read_csv(
+  '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/OGlcNAc_GO.csv'
+)
+
+barplot_GO_OGlcNAc <- OGlcNAc_GO |> 
+  filter(Description %in% c(
+    'regulation of transcription by RNA polymerase II',
+    'positive regulation of metabolic process',
+    'regulation of DNA-templated transcription',
+    'nucleus',
+    'nucleoplasm',
+    'chromatin'
+  )) |> 
+  ggplot() +
+  geom_bar(
+    aes(
+      x = fct_reorder(Description, -log10(pvalue)), 
+      y = -log10(pvalue)
+    ),
+    fill = '#1f77b4', color = 'transparent', stat = 'identity'
+  ) +
+  labs(x = '', y = '') +
+  coord_flip() +
+  theme_classic() +
+  theme(
+    axis.text = element_text(size = 8, color = 'black')
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/Retreat2025/Figures/barplot_GO_OGlcNAc.tif',
+  plot = barplot_GO_OGlcNAc,
+  height = 2, width =  3.5, units = 'in', dpi = 300
+)
+
+# O-GlcNAc on tyrosine residue
+library(tidyverse)
+
+Preliminary_Results_bonafide <- read_csv(
+  '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/Preliminary_Results_bonafide.csv'
+)
+
+OGlcNAc_Tyrosine_Level_1 <- Preliminary_Results_bonafide |> 
+  filter(Total.Glycan.Composition %in% c('HexNAt(1)', 'HexNAt(1)TMT6plex(1)')) |> 
+  filter(str_detect(Assigned.Modifications, 'Y')) |> 
+  filter(Confidence.Level == 'Level1') |> 
+  mutate(site_number_insequence = as.numeric(str_extract(Site.Probabilities, '(?<=\\[)\\d+'))) |> 
+  mutate(site_number_inprotein = Protein.Start + site_number_insequence -1) |> 
+  mutate(site_index = paste(Protein.ID, site_number_inprotein, sep = "_"))
+
+write_csv(
+  OGlcNAc_Tyrosine_Level_1,
+  file = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/OGlcNAc_Tyrosine_Level_1.csv'
+)
+
+OGlcNAc_Tyrosine_site_index <- OGlcNAc_Tyrosine_Level_1 |> 
+  distinct(site_index) |> 
+  pull()
+
+# import O-GlcNAc on tyrosine residue from literature
+# https://www.pnas.org/doi/10.1073/pnas.2409501121, Dataset S03 (XLSX)
+library(readxl)
+library(tidyverse)
+
+OGlcNAc_Tyrosine_site_literature <- read_xlsx(
+  '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/pnas.2409501121.sd03.xlsx',
+  range = "A5:D2835",
+  col_names = c('UniProt_Accession', 'Protein.Description', 'Site.Position', 'Residue')
+) |> 
+  filter(Residue == 'Y') |> 
+  mutate(site_index = paste(UniProt_Accession, Site.Position, sep = '_'))
+
+write_csv(
+  OGlcNAc_Tyrosine_site_literature,
+  file = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_HEK293T/OGlyco_HEK293T_Preliminary_Results/OGlcNAc_Tyrosine_site_literature.csv'
+)
+
+OGlcNAc_Tyrosine_site_literature_index <- OGlcNAc_Tyrosine_site_literature |> 
+  distinct(site_index) |> 
+  pull()
+
+# venn diagram
+library(VennDiagram)
+
+venn.diagram(
+  x = list(
+    "My Data" = OGlcNAc_Tyrosine_site_index,
+    "Literature" = OGlcNAc_Tyrosine_site_literature_index
+  ),
+  filename = "/Volumes/cos-lab-rwu60/Longping/Retreat2025/Figures/OGlcNAc_Tyrosine_Venn.tiff",
+  imagetype = "tiff",
+  resolution = 300,
+  cex = 4,
+  cat.cex = 4,
+  cat.pos = c(0, 180),
+  cat.dist = c(0.01, 0.01),
+  
+  lty = "blank",
+  fill = c("blue", "red"),
+  alpha = 0.3,
+  scaled = TRUE
+)
