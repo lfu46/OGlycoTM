b984653d7ff72431ae1f48edf7528aa7d20aa383
Longping Fu <lpfu46@gatech.edu>
2025-12-25 13:22:58 -0500

    update analysis for HepG2 and Jurkat cells

--- data_analysis/OGlycoTM_Jurkat/data_import.R
+++ data_analysis/OGlycoTM_Jurkat/data_import.R
@@ -111,4 +111,100 @@ Total_GlycoPSM_OGalNAc_by_raw_file <- OGlyco_Jurkat_bonafide |>
   count() |> 
   ungroup()
 
+## plot for preliminary results
+# Total glycoPSMs and unique glycoproteins
+Total_GlycoPSMs_Unique_Glycoproteins <- bind_rows(
+  GlycoPSM_Glycoprotein_Results_OGlcNAc %>% mutate(glycosylation = 'O-GlcNAc'),
+  GlycoPSM_Glycoprotein_Results_OGalNAc %>% mutate(glycosylation = 'O-GalNAc')
+)
 
+Barplot_Total_GlycoPSMs_Unique_Glycoproteins <- Total_GlycoPSMs_Unique_Glycoproteins |> 
+  mutate(glycosylation = factor(glycosylation, levels = c("O-GlcNAc", "O-GalNAc"))) |> 
+  ggplot(aes(x = glycosylation, y = number, fill = category)) +
+  geom_bar(stat = 'identity', position = position_dodge(width = 0.8), width = 0.7) +
+  geom_text(aes(label = number), 
+            position = position_dodge(width = 0.8), 
+            vjust = -0.1,
+            size = 2.5) +
+  scale_fill_manual(values = c('Total GlycoPSM' = '#2C7BB6', 
+                               'Unique Glycoprotein' = '#D7191C')) +
+  labs(x = NULL,
+       y = 'No. of O-glycan identifications',
+       fill = NULL) +
+  theme_bw() +
+  theme(
+    legend.position = 'right',
+    axis.text = element_text(color = 'black', size = 7, family = 'arial'),
+    axis.title = element_text(color = 'black', size = 7, family = 'arial'),
+    legend.text = element_text(color = 'black', size = 5, family = 'arial'),
+    legend.key.size = unit(0.1, 'in')
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_Jurkat/EThcD_OPair_Search_1/Barplot_Total_GlycoPSMs_Unique_Glycoproteins.tiff',
+  plot = Barplot_Total_GlycoPSMs_Unique_Glycoproteins,
+  width = 3, height = 2, units = 'in', dpi = 300
+)
+
+# Total glycoPSMs by raw file
+# O-GlcNAc
+barplot_Total_GlycoPSM_OGlcNAc_by_raw_file <- Total_GlycoPSM_OGlcNAc_by_raw_file |> 
+  mutate(
+    # Extract the fraction number from raw_file
+    fraction = as.numeric(str_extract(raw_file, "(?<=_OG_)\\d+(?=_)")),
+    # Create new label
+    x_label = paste0("F", fraction)
+  ) |> 
+  # Arrange by fraction number to ensure correct order
+  arrange(fraction) |> 
+  # Convert x_label to factor with levels in the arranged order
+  mutate(x_label = factor(x_label, levels = x_label)) |>
+  ggplot(aes(x = x_label, y = n)) +
+  geom_bar(stat = "identity") +
+  labs(
+    x = "Fraction",
+    y = "No. of total O-GlcNAc PSMs"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(color = 'black', size = 7, family = 'arial', angle = 30),
+    axis.text.y = element_text(color = 'black', size = 7, family = 'arial'),
+    axis.title = element_text(color = 'black', size = 7, family = 'arial')
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_Jurkat/EThcD_OPair_Search_1/barplot_Total_GlycoPSM_OGlcNAc_by_raw_file.tiff',
+  plot = barplot_Total_GlycoPSM_OGlcNAc_by_raw_file,
+  width = 3, height = 2.5, units = 'in', dpi = 300
+)
+
+# O-GalNAc
+barplot_Total_GlycoPSM_OGalNAc_by_raw_file <- Total_GlycoPSM_OGalNAc_by_raw_file |> 
+  mutate(
+    # Extract the fraction number from raw_file
+    fraction = as.numeric(str_extract(raw_file, "(?<=_OG_)\\d+(?=_)")),
+    # Create new label
+    x_label = paste0("F", fraction)
+  ) |> 
+  # Arrange by fraction number to ensure correct order
+  arrange(fraction) |> 
+  # Convert x_label to factor with levels in the arranged order
+  mutate(x_label = factor(x_label, levels = x_label)) |>
+  ggplot(aes(x = x_label, y = n)) +
+  geom_bar(stat = "identity") +
+  labs(
+    x = "Fraction",
+    y = "No. of total O-GalNAc PSMs"
+  ) +
+  theme_bw() +
+  theme(
+    axis.text.x = element_text(color = 'black', size = 7, family = 'arial', angle = 30),
+    axis.text.y = element_text(color = 'black', size = 7, family = 'arial'),
+    axis.title = element_text(color = 'black', size = 7, family = 'arial')
+  )
+
+ggsave(
+  filename = '/Volumes/cos-lab-rwu60/Longping/OGlycoTM_Jurkat/EThcD_OPair_Search_1/barplot_Total_GlycoPSM_OGalNAc_by_raw_file.tiff',
+  plot = barplot_Total_GlycoPSM_OGalNAc_by_raw_file,
+  width = 3, height = 2.5, units = 'in', dpi = 300
+)
